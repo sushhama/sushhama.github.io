fa548df442548f354e677356d0c7317524d020b9
Ramratan Gupta <ramratan.gupta@gmail.com>
2026-01-25 22:11:51 +0530

    name

--- src/components/Name.js
+++ src/components/Name.js
@@ -15,7 +15,7 @@ export default function Name() {
       'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5, 'f': 8, 'g': 3, 'h': 5,
       'i': 1, 'j': 1, 'k': 2, 'l': 3, 'm': 4, 'n': 5, 'o': 7, 'p': 8,
       'q': 1, 'r': 2, 's': 3, 't': 4, 'u': 6, 'v': 6, 'w': 6, 'x': 6,
-      'y': 1, 'z': 7
+      'y': 1, 'z': 7,'0':0,'1':1,'2':2,'3':3,'4':4,'5':5,'6':6,'7':7,'8':8,'9':9
     }
     const nameStr = name.replace(" ","").toLocaleLowerCase().split("");
     console.log(nameStr)
